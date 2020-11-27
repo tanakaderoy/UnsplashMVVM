d271ef061f940655f8d2f61c77c9cfb08acd9659
tanakaderoy <tanaka.deroy@gmail.com>
2020-11-27 01:42:41 -0500

    added model class for the unsplash api json

---
+++ app/src/main/java/com/tanaka/mazivanhanga/unsplashmvvm/UnsplashPhoto.kt
@@ -0,0 +1,39 @@
+package com.tanaka.mazivanhanga.unsplashmvvm
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+
+/**
+ * Created by Tanaka Mazivanhanga on 11/27/2020
+ */
+
+@Parcelize
+data class UnsplashPhoto(
+    val id: String,
+    val description: String?,
+    val urls: UnsplashPhotoUrls
+) : Parcelable {
+    @Parcelize
+    data class UnsplashPhotoUrls(
+        val raw: String,
+        val full: String,
+        val regular: String,
+        val small: String,
+        val thumb: String
+    ) : Parcelable
+
+    @Parcelize
+    data class UnsplashUser(
+        val username: String,
+        val name: String
+
+    ) : Parcelable {
+        val attributionUrl get() = "https://unsplash.com/${username}?utm_source=AndroidMVVM&utm_medium=referral"
+    }
+
+
+}
+
+
+
