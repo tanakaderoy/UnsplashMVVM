8f384af11dba8f71f4cb22fb09f4a8434cb2e28d
tanakaderoy <tanaka.deroy@gmail.com>
2020-11-27 21:36:55 -0500

    implement the details fragment

--- app/src/main/java/com/tanaka/mazivanhanga/unsplashmvvm/MainActivity.kt
+++ app/src/main/java/com/tanaka/mazivanhanga/unsplashmvvm/MainActivity.kt
@@ -1,13 +1,31 @@
 package com.tanaka.mazivanhanga.unsplashmvvm
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_main) as NavHostFragment
+
+        navController = navHostFragment.findNavController()
+        val appBarConfig = AppBarConfiguration(navGraph = navController.graph)
+
+        setupActionBarWithNavController(navController = navController, configuration = appBarConfig)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/com/tanaka/mazivanhanga/unsplashmvvm/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/tanaka/mazivanhanga/unsplashmvvm/ui/details/DetailsFragment.kt
@@ -1,12 +1,76 @@
 package com.tanaka.mazivanhanga.unsplashmvvm.ui.details
 
+import android.content.Intent
+import android.graphics.drawable.Drawable
+import android.net.Uri
+import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.tanaka.mazivanhanga.unsplashmvvm.R
+import com.tanaka.mazivanhanga.unsplashmvvm.databinding.FragmentDetailsBinding
+import java.lang.String
 
 
 /**
  * Created by Tanaka Mazivanhanga on 11/27/2020
  */
-class DetailsFragment : Fragment(R.layout.fragment_details){
+class DetailsFragment : Fragment(R.layout.fragment_details) {
+    private val args by navArgs<DetailsFragmentArgs>()
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentDetailsBinding.bind(view)
+
+        binding.apply {
+            val photo = args.photo
+            Glide.with(this@DetailsFragment).load(photo.urls.full)
+                .error(R.drawable.ic_error)
+                .listener(object : RequestListener<Drawable> {
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        progressBar.isVisible = false
+                        return false
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        textViewCreater.isVisible = true
+                        textViewDescription.isVisible = !photo.description.isNullOrEmpty()
+                        progressBar.isVisible = false
+                        return false
+                    }
+
+                })
+                .centerCrop().into(imageView)
+            val uri = Uri.parse(photo.user.attributionUrl)
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            textViewCreater.apply {
+                text = String.format( context.getString(R.string.photo_attribution), photo.user.name)
+                setOnClickListener {
+                    context.startActivity(intent)
+                }
+                paint.isUnderlineText = true
+            }
+            textViewDescription.text = photo.description
+        }
+
+
+    }
 }
