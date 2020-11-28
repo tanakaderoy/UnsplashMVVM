22b3914b89ab68791dca460f95f38b7c1161184d
tanakaderoy <tanaka.deroy@gmail.com>
2020-11-27 20:20:43 -0500

    add searchview and options menu to gallery fragment

--- app/src/main/java/com/tanaka/mazivanhanga/unsplashmvvm/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/tanaka/mazivanhanga/unsplashmvvm/ui/gallery/GalleryFragment.kt
@@ -1,12 +1,16 @@
 package com.tanaka.mazivanhanga.unsplashmvvm.ui.gallery
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.tanaka.mazivanhanga.unsplashmvvm.R
 import com.tanaka.mazivanhanga.unsplashmvvm.databinding.FragmentGalleryBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_gallery.*
 
 
 /**
@@ -35,6 +39,30 @@ class GalleryFragment : Fragment(R.layout.fragment_gallery) {
         viewModel.photos.observe(viewLifecycleOwner) {
             adapter.submitData(viewLifecycleOwner.lifecycle, it)
         }
+
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_gallery, menu)
+        val searchItem = menu.findItem(R.id.action_search)
+        val searchView = searchItem.actionView as SearchView
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                query?.let {
+                    binding.recyclerView.scrollToPosition(0)
+                    viewModel.searchPhotos(query = it)
+                    searchView.clearFocus()
+                }
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return true
+            }
+
+        })
     }
 
     override fun onDestroyView() {
